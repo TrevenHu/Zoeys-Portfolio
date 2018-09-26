d51b7a2b56503a3d1b26dbb7ea509029e6f3c3d7
TrevenHu <hzy1994@bupt.edu.cn>
2018-09-26 14:17:05 -0700

    resyle the landing page, build the project section structure

--- public/javascripts/landing-page.js
+++ public/javascripts/landing-page.js
@@ -1,14 +1,54 @@
 function changeTextOver(){
-	var who = document.getElementsByClassName('who')[0];
-	if(who.innerHTML === "Who's she?"){
-		who.innerHTML = "Hello World!";
-		document.getElementsByClassName('header__side--front')[0].style.cssText = 'transform:rotateY(-180deg);';
- 		document.getElementsByClassName('header__side--back')[0].style.cssText = 'transform:rotateY(0deg);';
-	} else {
-		who.innerHTML = "Who's she?";
-		document.getElementsByClassName('header__side--front')[0].style.cssText = '';
- 		document.getElementsByClassName('header__side--back')[0].style.cssText = '';
+	document.getElementsByClassName('header__side--front')[0].style.cssText = 'transform:rotateY(-180deg);';
+	document.getElementsByClassName('header__side--back')[0].style.cssText = 'transform:rotateY(0deg);';
+}
+
+function smoothScroll(){
+	var anchor = document.getElementsByClassName('section-project')[0];
+	anchor.style.cssText = 'display:block;';
+	document.getElementById('anchor-natours').className = 'current';
+	document.getElementById('anchor-yelpdemo').className = '';
+	document.getElementById('anchor-little-things').className = '';
+	document.getElementById('natours').style.cssText = 'display:block;'
+	document.getElementById('yelpdemo').style.cssText = 'display:none;'
+	document.getElementById('little-things').style.cssText = 'display:none;'
+	anchor.scrollIntoView({behavior:"smooth"});
+}
+
+function navToProject(x){
+	window.location.href = '/project-'+x;
+}
+
+function nextProject(x) {
+	var cur = document.getElementById(x);
+	cur.style.cssText = 'display:none; animation:none initial;';
+	document.getElementById('anchor-'+x).className = '';
+	if(x === "natours"){
+		var next = document.getElementById('yelpdemo');
+		document.getElementById('anchor-yelpdemo').className = 'current';
+	} else if(x === "yelpdemo"){
+		var next = document.getElementById('little-things');
+		document.getElementById('anchor-little-things').className = 'current';
+	} else if(x === "little-things"){
+		var next = document.getElementById('natours');
+		document.getElementById('anchor-natours').className = 'current';
 	}
-	
- 	
+	next.style.cssText = 'display:block; animation:slideInLeft 1s backwards;';
+	var anchor = document.getElementsByClassName('section-project')[0];
+    anchor.scrollIntoView({behavior:"smooth"});
+}
+
+function prevProject(x) {
+	var cur = document.getElementById(x);
+	cur.style.cssText = 'display:none; animation:none initial;';
+	if(x === "yelpdemo"){
+		var prev = document.getElementById('natours');
+	} else if(x === "natours"){
+		var prev = document.getElementById('little-things');
+	} else if(x === "little-things"){
+		var prev = document.getElementById('yelpdemo');
+	}
+	prev.style.cssText = 'display:block; animation:slideInRight 1s forwards;';
+	var anchor = document.getElementsByClassName('section-project')[0];
+    anchor.scrollIntoView({behavior:"smooth"});
 }
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/project-natours', function(req, res) {
+  res.render('project-natours');
+});
+
 module.exports = router;
